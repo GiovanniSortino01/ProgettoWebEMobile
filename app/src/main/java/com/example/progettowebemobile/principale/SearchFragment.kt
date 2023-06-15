6b091ed5a9a3742c4abf044d37288f39e9798c74
alexi <sammataro.alex@gmail.com>
2023-06-15 04:21:39 +0200

    Registration

--- app/src/main/java/com/example/progettowebemobile/principale/SearchFragment.kt
+++ app/src/main/java/com/example/progettowebemobile/principale/SearchFragment.kt
@@ -6,14 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.progettowebemobile.R
+import com.example.progettowebemobile.databinding.FragmentSearchBinding
+import com.example.progettowebemobile.principale.search.HotelFragment
+import com.example.progettowebemobile.principale.search.MonumentFragment
+import com.example.progettowebemobile.principale.search.RestaurantFragment
 
 class SearchFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchBtnRestaurant.setOnClickListener {
+            val restaurantFragment = RestaurantFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.search_flFragment, restaurantFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.searchBtnHotel.setOnClickListener {
+            val hotelFragment = HotelFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.search_flFragment, hotelFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+        binding.searchBtnMonuments.setOnClickListener {
+            val monumentFragment = MonumentFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.search_flFragment, monumentFragment)
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
