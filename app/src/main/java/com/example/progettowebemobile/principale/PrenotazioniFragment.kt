ab03cc4651336717420dbe0e558f188e1d9ab032
alexi <sammataro.alex@gmail.com>
2023-06-25 22:44:31 +0200

    Registration

---
+++ app/src/main/java/com/example/progettowebemobile/principale/PrenotazioniFragment.kt
@@ -0,0 +1,27 @@
+package com.example.progettowebemobile.principale
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.progettowebemobile.R
+import com.example.progettowebemobile.databinding.FragmentPlaceBinding
+import com.example.progettowebemobile.databinding.FragmentPrenotazioniBinding
+
+class PrenotazioniFragment : Fragment() {
+
+    private lateinit var binding: FragmentPrenotazioniBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentPrenotazioniBinding.inflate(inflater, container, false)
+
+
+
+
+        return binding.root
+    }
+
+
+}
