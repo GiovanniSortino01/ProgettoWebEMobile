459ec3a835d27aefb8ed4afd2afca1cef927632e
alexi <sammataro.alex@gmail.com>
2023-06-25 02:15:09 +0200

    Registration

--- app/src/main/java/com/example/progettowebemobile/principale/search/RecyclerView/Place/PlaceFragment.kt
+++ app/src/main/java/com/example/progettowebemobile/principale/search/RecyclerView/Place/PlaceFragment.kt
@@ -7,12 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.db_connection.ClientNetwork
 import com.example.progettowebemobile.R
 import com.example.progettowebemobile.databinding.FragmentPlaceBinding
 import com.example.progettowebemobile.databinding.FragmentRecyclerviewSearchBinding
 import com.example.progettowebemobile.entity.Luogo
+import com.example.progettowebemobile.principale.NewTaskSheet
+import com.example.progettowebemobile.principale.TaskViewModel
 import com.example.progettowebemobile.principale.ViewPagerAdapter
 import com.example.progettowebemobile.principale.account.ItemsViewModelPost
 import com.google.gson.JsonObject
@@ -28,7 +31,7 @@ import java.io.IOException
 class PlaceFragment : Fragment() {
     private lateinit var binding: FragmentPlaceBinding
     private lateinit var luogo: Luogo
-
+    private lateinit var taskViewModel: TaskViewModel
     private var imagesList = mutableListOf<Int>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +45,11 @@ class PlaceFragment : Fragment() {
 
         binding.viewPager2.adapter = ViewPagerAdapter(imagesList)
         binding.viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        taskViewModel = ViewModelProvider(this)[TaskViewModel::class.java]
+        binding.searchFragmentBtnScegli.setOnClickListener {
+            NewTaskSheet().show(requireActivity().supportFragmentManager, "newTaskTag")
+        }
 
 
 
